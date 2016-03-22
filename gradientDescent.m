b51e1f7bf2198150011c265464509a3fe8d32cbe
JP <juhapekka.arimaa@gmail.com>
2016-03-22 20:37:28 +0200

    Simpler form

--- gradientDescent.m
+++ gradientDescent.m
@@ -1,15 +1,7 @@
 function [theta] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y);
 for iter = 1:num_iters
-    t1 = 0;
-    t2 = 0;
-        
-    for i = 1:m
-        t1 = t1 + (theta(1) + theta(2) * X(i,2) - y(i));
-        t2 = t2 + ((theta(1) + theta(2) * X(i,2) - y(i)) * X(i,2));
-    end
-    
-    theta(1) = (theta(1) - alpha * (1/m) * t1);
-    theta(2) = (theta(2) - alpha * (1/m) * t2);
+    h = X * theta;
+    theta = theta - alpha * (1/m) * ((h-y)' * X)';
 end
 end
