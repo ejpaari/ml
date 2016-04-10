480b5cb9743e1d584e511ca8ba1003179b63f78e
JP <juhapekka.arimaa@gmail.com>
2016-04-10 18:06:52 +0300

    ex3 neural network

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -10,26 +10,10 @@ num_labels = size(Theta2, 1);
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
+x1 = [ones(size(X,1), 1), X];
+a1 = sigmoid(x1 * Theta1');
+a1 = [ones(size(a1,1), 1), a1];
+a2 = sigmoid(a1 * Theta2');
+[v, p] = max(a2, [], 2);
 
 end
