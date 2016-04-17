b368146d719ff35e20e7c61005630b993078c2f6
JP <juhapekka.arimaa@gmail.com>
2016-04-17 13:09:03 +0300

    Completed ex4 backprop + regularization

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -12,7 +12,6 @@ function [J grad] = nnCostFunction(nn_params, ...
 % 
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
-%
 
 % Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
 % for our 2 layer neural network
@@ -28,41 +27,40 @@ J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
 
+% Part 1: Feedforward the neural network and return the cost in the
+%         variable J
 % Part 2: Implement the backpropagation algorithm to compute the gradients
-%         Theta1_grad and Theta2_grad. You should return the partial derivatives of
-%         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
-%         Theta2_grad, respectively. After implementing Part 2, you can check
-%         that your implementation is correct by running checkNNGradients
-%
-%         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
-%               binary vector of 1's and 0's to be used with the neural network
-%               cost function.
-%
-%         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
-%               first time.
-%
+%         After implementing Part 2, you can check that your implementation 
+%         is correct by running checkNNGradients
 % Part 3: Implement regularization with the cost function and gradients.
-%
-%         Hint: You can implement this around the code for
-%               backpropagation. That is, you can compute the gradients for
-%               the regularization separately and then add them to Theta1_grad
-%               and Theta2_grad from Part 2.
-%
 
-y_matrix = zeros(num_labels, m);
-for c = 1:num_labels
-    y_matrix(c, y==c) = 1;
+Y = zeros(m, num_labels);
+for i = 1:m
+    Y(i,:) = 1:num_labels == y(i);
 end
-y = y_matrix;
 
-a1 = [ones(1,size(X,1)); X'];
-a2 = [ones(1,size(X,1)); sigmoid(Theta1 * a1)];
-a3 = sigmoid(Theta2 * a2);
+a1 = X;
+z2 = [ones(m,1) a1] * Theta1';
+a2 = sigmoid(z2);
+z3 = [ones(m,1) a2] * Theta2';
+a3 = sigmoid(z3);
 
-J = 1/m * sum(sum(-y.*(log(a3))-(1-y) .* (log(1-a3))));
-J = J + lambda / (2*m) * (sum(sum(Theta1(:,2:end).^2))+sum(sum(Theta2(:,2:end).^2)));
+J = 1/m * sum(sum(-Y.*(log(a3))-(1-Y) .* (log(1-a3))));
+J = J + lambda / (2*m) * (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)));
+
+d3 = a3 - Y;
+d2 = (d3 * Theta2(:,2:end)) .* sigmoidGradient(z2);
+
+for i = 1:m
+  Theta1_grad = Theta1_grad + d2(i,:)' * [1 a1(i,:)];
+  Theta2_grad = Theta2_grad + d3(i,:)' * [1 a2(i,:)];
+end;
+
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + lambda * Theta1(:,2:end);
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + lambda * Theta2(:,2:end);
+
+Theta1_grad = Theta1_grad ./ m;
+Theta2_grad = Theta2_grad ./ m;
 
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
