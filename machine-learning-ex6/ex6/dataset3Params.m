1b379047fa1e21b7ae258fd7f06420d748a82c52
JP <juhapekka.arimaa@gmail.com>
2016-04-27 18:55:29 +0300

    Support vector machines

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -7,11 +7,9 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
-% You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+C = 0;
+sigma = 0.0;
 
-% ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
@@ -22,13 +20,19 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+a = [0.01 0.03 0.1 0.3 1 3 10 30];
+minError = 999999999;
 
-
-
-
-
-
-
-% =========================================================================
-
+for i=1:length(a)
+    for j=1:length(a)
+        model = svmTrain(X, y, a(i), @(x1, x2) gaussianKernel(x1, x2, a(j)));
+        predictions = svmPredict(model, Xval);
+        predError = mean(double(predictions ~= yval));
+        if predError < minError
+            minError = predError;
+            C = a(i);
+            sigma = a(j);
+        end
+    end
+end
 end
